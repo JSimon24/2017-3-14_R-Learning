f36ccbe9942ff42faf6c899ef46ed31c46febfa1
Jillian Simon <jilliansimon24@gmail.com>
2017-03-14 11:05:58 +0000

    updated scripts from part 1 of class

--- scripts/00_scratchpad.R
+++ scripts/00_scratchpad.R
@@ -7,3 +7,23 @@ x <- log(2)
 x
 x <- 1/30
 y <- 1:5
+y * 2
+y[3]
+ls()
+rm(x)
+rm(list=ls())
+installed.packages("tidyr")
+library(tidyr)
+install.packages("tidyr")
+
+library(tidyr)
+
+# this is a comment
+# Use: to tell me what I am doing with this bit of code
+
+rnorm(10)
+results <- boxplot(rnorm(10))
+results
+
+example(boxplot)
+
